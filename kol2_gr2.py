e757fe72d8c0f54e21fafd08b47e318dde12b537
maciejmarzeta <94maciekm@gmail.com>
2016-11-10 19:29:14 +0100

    final version

--- kol2_gr2.py
+++ kol2_gr2.py
@@ -14,62 +14,112 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+from __future__ import division
 
 class Student(object):
     def __init__(self, stud_id, name = " ", surname = " "):
         self.stud_id = stud_id
         self.name = name
         self.surname = surname
-
         self.scores = []
-        self.scores_sum = 0
-        self.average_score = 0
+        self.lessons = []
 
-        self.lesson_num = []    #numer lekcji to indeks a obecnosc to boolean
-        self.curr_lesson = 0
 
     def add_score(self, score):
-        self.scores_counter += 1
-        self.scores_sum += score
-        self.average_score = self.scores_sum / self.scores_counter
-
-    def add_attendance(self, lesson_num, is_attend):
-        self.curr_lesson += 1
-        self.lesson_num[self.curr_lesson] = bool(is_attend)
-
-    def get_attandance_percent(self):
-        if self.curr_lesson != 0:
-            res = sum(self.lesson_num) / self.curr_lesson
+        if isinstance(score, int) and score <=6 and score >=1:
+            self.scores.append(score)
         else:
-            res = 0
-        return res
+            raise ValueError("Score value is incorrect, should be int <= 6 and >= 1")
+
+
+    def add_attendance(self, is_attend):
+        self.lessons.append(bool(is_attend))
+
+
+    def get_attendance_percent(self):
+        if self.lessons.__len__() == 0:
+            return 0
+        else:
+            return (sum(self.lessons) / self.lessons.__len__()) * 100
+
+
+    def get_average_score(self):
+        if self.scores.__len__() == 0:
+            return 0
+        else:
+            return sum(self.scores) / self.scores.__len__()
+
 
     def __str__(self):
-        str = "Student id: " + self.student_id " name: " + self.name + " surname: " + self.surname, "/n"\
-            "score average: " + self.average_score + "attandance %: " + self.get_attandance_percent()
-        return str
+        return "Student's id: " + str(self.stud_id)\
+               + ", Name: " + self.name + " " + self.surname\
+               + " Attendance %: " + str(self.get_attendance_percent())\
+               + "% scores average: " + str(self.get_average_score())
+
 
 class StudentsGroup(object):
     def __init__(self, group_id, group_name = " "):
-        self.group = []
+        self.students = []
         self.name = group_name
         self.id = group_id
-        self.size = 0
+
 
     def add_student(self, stud):
-        self.group[self.size] = stud
-        self.size += 1
+        self.students.append(stud)
+        print stud.name, " ", stud.surname, " Has been added to StudentGroup ", self.name
+
+
+    def remove_student(self, id):
+        for i in range(self.students.__len__()):
+            if self.students[i].stud_id == id:
+                print "Student with id: ", id, "Has been removed"
+                self.students.remove(self.students[i])
+                return
+        raise Exception("Student with that id doesnt exist")
+
+
+    def get_group_average_score(self):
+        if self.students.__len__() != 0:
+            avr = 0
+            for i in range(self.students.__len__()):
+                avr += self.students[i].get_average_score()
+            return avr / self.students.__len__()
+        else:
+            return 0
+
 
     def __str__(self):
-        str = "StudentGroup id: ", self.id, " name: ", self.name, " number of students: ", self.size
-        for i in range(self.group.__len__()):
-            str += self.group[i]
-        return str
+        res = "StudentGroup id: " + str(self.id)\
+              + " name: " + self.name\
+              + " number of students: " + str(self.students.__len__())
+        for i in range(self.students.__len__()):
+            res += "\n" + str(self.students[i])
+        res += "\nAverage score of students in " + self.name + " is: " + str(self.get_group_average_score())
+        return res
 
 
-a = Student(1, "Jan", "Naj")
-b = Student(2, "Ola", "Alo")
+if __name__ == "__main__":
+    st1 = Student(1, "Jan", "Kowalski")
+    st2 = Student(2, "Ola", "Mak")
+    st3 = Student(3, "Ala", "Baca")
 
-print a
+    sg = StudentsGroup(1, "Klasa")
 
+    sg.add_student(st1)
+    sg.add_student(st2)
+    sg.add_student(st3)
 
+    st1.add_score(3)
+    st1.add_score(4)
+    st1.add_attendance(0)
+    st1.add_attendance(0)
+    st1.add_attendance(1)
+    print st1
+
+    st2.add_score(1)
+    st2.add_score(4)
+    st2.add_attendance(True)
+    st2.add_attendance(1)
+    print st2
+
+    print sg
